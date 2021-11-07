81fe76cceb986948fb926ff62925da54d874dac6
Huzaifa Mohammed <huzirizwan@gmail.com>
2021-11-07 01:39:36 -0500

    name prompt issue

--- assets/js/game.js
+++ assets/js/game.js
@@ -106,8 +106,23 @@ var shop = function() {
     }
 
 };
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    // ***************************************
+    // ADD LOOP HERE WITH PROMPT AND CONDITION
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    // ***************************************
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 var playerInfo = {
-    Name: window.prompt("what is your robots name ?"),
+    Name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10
@@ -143,6 +158,7 @@ var startGame = function() {
 
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
             var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health = randomNumber(40, 60);
             playerInfo.health = 100;
